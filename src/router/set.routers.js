90d027ac35b57eb8818907164e106d52cfbf45a7
cascodenotch <catalinaarciniegassaenz@gmail.com>
2024-12-14 16:51:38 +0100

    endpoints y verbos

--- src/router/set.routers.js
+++ src/router/set.routers.js
@@ -2,9 +2,12 @@ const { Router } = require("express");
 const router = Router();
 const setCtrl = require("../controller/set.controller");
 
+router.get("/mis-sets", setCtrl.getSet);
+router.delete("/mis-sets", setCtrl.deleteSet);
 router.get("/set", setCtrl.getSet);
 router.put("/set", setCtrl.putSet);
 router.post("/set", setCtrl.postSet);
-router.delete("/set", setCtrl.deleteSet);
+router.delete("/set/song", setCtrl.deleteSong);
+router.get("/set/analisis", setCtrl.songAnalysis);
 
 module.exports = router; 
--- src/router/song.routers.js
+++ src/router/song.routers.js
@@ -2,5 +2,7 @@ const { Router } = require("express");
 const router = Router();
 const songCtrl = require("../controller/song.controller");
 
+router.get("/mis-canciones", songCtrl.getRecentlyPlayed);
+router.get("/recomendaciones", songCtrl.getRecommendations);
 
 module.exports = router; 
